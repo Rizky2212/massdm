8d038f64023d7c6c560610f9c9f5bc6d68508601
Rizky2212 <68724189+Rizky2212@users.noreply.github.com>
2020-07-24 12:32:39 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 echo Don't forget to run install_dep.bat prior to starting^!
 title MassDM Discord Bot
-REM set "BOT_TOKEN=YOUR TOKEN"
+REM set "BOT_TOKEN=NzM2MDc5OTA0MDc2MDcwOTYy.Xxpl1A.5GTy1ZsLbP5OPHMeu6EK3HIqMxg"
 node app.js --harmony
 pause
